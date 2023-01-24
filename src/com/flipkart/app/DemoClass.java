fbb402a99e3fb79fffb87a2735fd24f21755e0f3
Harsh Doshi <doshiharsh26@gmail.com>
2023-01-24 14:28:46 +0530

    sts

---
+++ src/com/flipkart/app/DemoClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.flipkart.app;
+
+/**
+ * @author harsh.doshi
+ *
+ */
+public class DemoClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
